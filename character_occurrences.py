ddb0f9caf438581795707fb39a518aa418e84340
Shanaeh <shanaeh@stanford.edu>
2020-11-19 22:06:35 -0800

    character occurrences data + script

--- character_occurrences.py
+++ character_occurrences.py
@@ -3,31 +3,35 @@ import re
 import pandas as pd
 
 import nltk
-from nltk.corpus import stopwords
-cachedStopWords = stopwords.words("english")
 
 #variables for this script
-my_main_directory = f"{os.getcwd()}/corpus/"
+my_main_directory = f"{os.getcwd()}/nested_paragraphs/"
 encoding = "utf-8" # text file encoding: utf-8
 
 #char names
 sas_chars = [["Elinor", "Miss Dashwood"],
-            ["Marianne"],
-            ["Brandon"],
+            ["Marianne", "Miss Dashwoods"], #tag Marianne, the younger, when paired
+            ["Brandon", "the Colonel"],
             ["Willoughby"],
             ["Edward", "Mr. Ferrars"],
             ["Miss Grey", "Sophia"],
-            ["Steele"],
-            ["Mrs. Jennings"]]
+            ["Lucy", "Miss Steeles"], #tag Lucy, the younger, when paired
+            ["Mrs. Jennings"]
+            ]
+sas_names = ["Elinor Dashwood", "Marianne Dashwood", "Colonel Brandon", "John Willoughby", "Edward Ferrars", "Sophia Grey", "Lucy Steele", "Mrs. Jennings"]
 
-pap_chars = [["Elizabeth", "Lizzie"],
-             ["Jane", "Ms. Bennet"],
-             ["Wickham"],
-             ["Darcy"],
-             ["Bingley", "Charles"],
-             ["Charlotte", "Ms. Lucas", "Mrs. Collins"],
-             ["Lady Catherine", "De Bourgh"],
-             ["Mr. Collins", "WILLIAM COLLINS"]]
+pap_chars = [["Elizabeth", "Lizzy"],
+            ["Darcy"],
+            ["Jane", "Miss Bennet"],
+            ["Bingley", "Charles"], #Unfortunately this will also include 87 references to Miss Bingley
+            ["Wickham"],
+            ["Charlotte", "Miss Lucas", "Mrs. Collins"],
+            ["Lady Catherine", "De Bourgh"],
+            ["Mr. Collins", "WILLIAM COLLINS"],
+            ["Lydia", "Mrs. Wickham"],
+            ["Miss Bingley", "Caroline"]
+            ]
+pap_names = ["Elizabeth Bennet", "Fitzwilliam Darcy", "Jane Bennet", "Charles Bingley", "George Wickham", "Charlotte Lucas", "Lady Catherine", "Mr. Collins", "Lydia Bennet", "Caroline Bingley"]
 
 na_chars = [["Catherine", "Miss Morland"],
             ["Henry", "Mr. Tilney"],
@@ -36,7 +40,10 @@ na_chars = [["Catherine", "Miss Morland"],
             ["James", "Mr. Morland"],
             ["General Tilney", "the general"], #<5 uses of "the general" for non-character uses
             ["Captain Tilney", "Frederick"],
-            ["Mrs. Allen", "Mr. Allen"]]
+            ["Mrs. Allen"], 
+            ["Mr. Allen"]
+            ]
+na_names = ["Catherine Morland", "Henry Tilney", "Eleanor Tilney", "Isabella Thorpe", "James Morland", "General Tilney", "Captain Tilney", "Mrs. Allen", "Mr. Allen"]
 
 mp_chars = [["Fanny", "Miss Price"],
             ["Edmund"],
@@ -44,76 +51,109 @@ mp_chars = [["Fanny", "Miss Price"],
             ["Mary", "Miss Crawford"],
             ["Mrs. Norris"],
             ["Sir Thomas"],
+            ["Lady Bertram"],
             ["Maria", "Mrs. Rushworth", "Miss Bertram"], #eldest daughter is Miss until married
-            ["Julia", "Miss Bertrams"]] #Only tag Julia when the Miss Bertrams are mentioned together
+            ["Julia", "Miss Bertrams"] #Tag Julia, the younger, when the Miss Bertrams are mentioned together
+            ] 
+mp_names = ["Fanny Price", "Edmund Bertram", "Henry Crawford", "Mary Crawford", "Mrs. Norris", "Sir Thomas", "Lady Bertram", "Maria Bertram", "Julia Bertram"]
 
-emma_chars = [["Emma"], 
-             ["Knightley"],
-             ["Frank", "Mr. Churchill"],
-             ["Jane"],
-             ["Harriet"], 
-             ["Bates"],
-             ["Mrs. Weston", "Taylor"],
-             ["Elton"]]
+emma_chars = [["Emma", "Miss Woodhouse"], 
+            ["Knightley"],
+            ["Frank", "Mr. Churchill"],
+            ["Jane", "Miss Fairfax"],
+            ["Harriet", "Miss Smith"], 
+            ["Robert", "Mr. Martin"],
+            ["Miss Bates"],
+            ["Mrs. Weston", "Miss Taylor"],
+            ["Mr. Elton"],
+            ["Mrs. Elton"]
+            ]
+emma_names = ["Emma Woodhouse", "George Knightley", "Frank Churchill", "Jane Fairfax", "Harriet Smith", "Robert Martin", "Miss Bates", "Mrs. Weston", "Mr. Elton", "Mrs. Elton"]
 
 p_chars = [["Anne"],
             ["Frederick", "Wentworth"],
             ["Mr Elliot"],
             ["Mary", "Mrs Musgrove"],
             ["Elizabeth", "Miss Elliot"],
+            ["Lady Russell"],
             ["Walter"],
             ["Mrs Clay"],
-            ["Louisa"],
-            ["Henrietta"]]
+            ["Louisa", "Miss Musgrove"],
+            ["Henrietta", "Miss Musgroves"], #Louisa and Henrietta referred to as a pair
+            ["Mrs Smith"]
+            ]
+p_names = ["Anne Elliot", "Frederick Wentworth", "William Elliot", "Mrs. Musgrove", "Elizabeth Elliot", "Lady Russell", "Sir Walter", "Mrs. Clay", "Louisa Musgrove", "Henrietta Musgrove", "Mrs. Smith"]
 
-#pos script inspired and drawn from https://github.com/p-mckenzie/example-projects/blob/master/Jane%20Austen%20Analysis.ipynb
+#character occurrence script inspired from https://github.com/p-mckenzie/example-projects/blob/master/Jane%20Austen%20Analysis.ipynb
+def count_char_occurrences(filename, char_list, char_standardized_names):
+    with open(my_main_directory + filename) as file:
+        novel_nested_paragraphs = json.load(file)
+    char_occurrences = []
+    for name in char_standardized_names:
+        char_occurrences.append({"name": name, "values": []})
 
-with open(my_main_directory + "Austen_SenseAndSensibility.txt") as file:
-    sas_data = file.read()
-with open(my_main_directory + "Austen_PrideAndPrejudice.txt") as file:
-    pap_data = file.read()
-with open(my_main_directory + "Austen_NorthangerAbbey.txt") as file:
-    na_data = file.read()
-with open(my_main_directory + "Austen_MansfieldPark.txt") as file:
-    mp_data = file.read()
-with open(my_main_directory + "Austen_Emma.txt") as file:
-    emma_data = file.read()
-with open(my_main_directory + "Austen_Persuasion.txt") as file:
-    p_data = file.read()
+    current_char_index = 0
+    for char_nicknames in char_list:
+        for chapter in novel_nested_paragraphs:
+            chapter_count = 0
+            chapter_values = {"chapter": chapter["chapter"], "count": 0, "paragraph_counts": []}
 
-#remove character names/titles as well as stopwords
-for name in {re.sub(r"\W", '', new_item) for sublist in emma_chars+pap_chars+sas_chars+p_chars+na_chars+mp_chars for item in sublist for new_item in item.split()}:
-    cachedStopWords.append(name.lower())
-cachedStopWords.append("charles")
+            for paragraph in chapter["paragraphs"]:
+                paragraph_count = 0
+                sentence_list =  nltk.sent_tokenize(paragraph)
+                #Count max 1 mention per sentence to reduce exaggeration / overlap (e.g. Mrs. Bennet muttering Lydia several times in one sentence)
+                for sentence in sentence_list:
+                    for name in char_nicknames:
+                        if name in sentence:
+                            paragraph_count += 1
+                            break
+                
+                chapter_count += paragraph_count
+                chapter_values["paragraph_counts"].append(paragraph_count)
 
-def tag_text(message):
-    return nltk.pos_tag([word for word in re.findall(r"[a-z']+", message.lower()) 
-			 if word not in cachedStopWords])
+            chapter_values["count"] = chapter_count
+            char_occurrences[current_char_index]["values"].append(chapter_values)
+        
+        current_char_index += 1
+    filename_ending_len = len("nested_paragraphs.json") + 1
+    with open(filename[:-filename_ending_len] + "_character_occurrences.json", "w") as outfile:
+      json.dump(char_occurrences, fp=outfile, sort_keys=True, indent=4)
 
-def condense_pos(df):
-    df.loc[df['pos'].isin(['JJS', 'JJR']), 'pos'] = 'JJ'
-    df.loc[df['pos'].isin(['NNP', 'NNPS', 'NNS']), 'pos'] = 'NN'
-    df.loc[df['pos'].isin(['RBR', 'RBS']), 'pos'] = 'RB'
-    df.loc[df['pos'].isin(['VBD' 'VBG', 'VBN', 'VBP', 'VBZ']), 'pos'] = 'VB'
-    return df.loc[df['pos'].isin(['JJ', 'NN', 'RB', 'VB'])]
 
-#sas_words = pd.DataFrame(tag_text(sas_data), columns=['word', 'pos'])
-#pap_words = pd.DataFrame(tag_text(pap_data), columns=['word', 'pos'])
-#na_words = pd.DataFrame(tag_text(na_data), columns=['word', 'pos'])
-#mp_words = pd.DataFrame(tag_text(mp_data), columns=['word', 'pos'])
-#emma_words = pd.DataFrame(tag_text(emma_data), columns=['word', 'pos'])
-#p_words = pd.DataFrame(tag_text(p_data), columns=['word', 'pos'])
+#since this script will look for substrings, which can be error-prone with last name references 
+#as male characters are often referred to by their last name, even if they have female relations with Miss, Mr, Mrs
+#there will be a few overlaps; normalization + smoothing with d3 will hopefuly reduce reader confusion
 
-#sas_words = condense_pos(sas_words).groupby(['pos', 'word']).size().reset_index().sort_values(0, ascending=False).groupby('pos').head(5).sort_values(['pos', 0], ascending=[True, False])
-#pap_words = condense_pos(pap_words).groupby(['pos', 'word']).size().reset_index().sort_values(0, ascending=False).groupby('pos').head(5).sort_values(['pos', 0], ascending=[True, False])
-#na_words = condense_pos(na_words).groupby(['pos', 'word']).size().reset_index().sort_values(0, ascending=False).groupby('pos').head(5).sort_values(['pos', 0], ascending=[True, False])
-#mp_words = condense_pos(mp_words).groupby(['pos', 'word']).size().reset_index().sort_values(0, ascending=False).groupby('pos').head(5).sort_values(['pos', 0], ascending=[True, False])
-#emma_words = condense_pos(emma_words).groupby(['pos', 'word']).size().reset_index().sort_values(0, ascending=False).groupby('pos').head(5).sort_values(['pos', 0], ascending=[True, False])
-#p_words = condense_pos(p_words).groupby(['pos', 'word']).size().reset_index().sort_values(0, ascending=False).groupby('pos').head(5).sort_values(['pos', 0], ascending=[True, False])
+# count_char_occurrences("Austen_SenseAndSensibility_nested_paragraphs.json", sas_chars, sas_names)
+# count_char_occurrences("Austen_PrideAndPrejudice_nested_paragraphs.json", pap_chars, pap_names)
+# count_char_occurrences("Austen_NorthangerAbbey_nested_paragraphs.json", na_chars, na_names)
+# count_char_occurrences("Austen_MansfieldPark_nested_paragraphs.json", mp_chars, mp_names)
+# count_char_occurrences("Austen_Emma_nested_paragraphs.json", emma_chars, emma_names)
+# count_char_occurrences("Austen_Persuasion_nested_paragraphs.json", p_chars, p_names)
 
-#print(sas_words)
-#print(pap_words)
-#print(na_words)
-#print(mp_words)
-#print(emma_words)
-#print(p_words)
+
+########################
+#fix the Charles Bingley and Miss Bingley name overlap; even in text-mining Miss Bingley seeks to sabotage :)
+def clean_pap(): #get temp file from removing "Caroline" (17 occurrences) and only using "Miss Bingley" in the char mappings
+    with open(os.getcwd() + "/" + "Austen_PrideAndPrejudice_character_occurrences_TEMP.json") as file:
+        char_groupings = json.load(file)
+    
+    bingley_grouping = char_groupings[3]
+    caroline_grouping = char_groupings[9]
+
+    current_chapter_index = 0
+    for chapter_value in bingley_grouping["values"]:
+        if caroline_grouping["values"][current_chapter_index]["count"]:
+            chapter_value["count"] -= caroline_grouping["values"][current_chapter_index]["count"]
+            curr_paragraph_index = 0
+            for paragraph in chapter_value["paragraph_counts"]:
+                chapter_value["paragraph_counts"][curr_paragraph_index] -= caroline_grouping["values"][current_chapter_index]["paragraph_counts"][curr_paragraph_index]
+                curr_paragraph_index += 1
+        current_chapter_index += 1
+    
+    char_groupings[3] = bingley_grouping
+
+    with open("pap_cleaned_to_copy_over.json", "w") as outfile:
+      json.dump(char_groupings, fp=outfile, sort_keys=True, indent=4)
+
+# clean_pap()
